21a1db22a6e2038e6629b45ff220d951d2e70af2
Nguyễn Quang Vinh <31350719+NguyenQuangVinh123@users.noreply.github.com>
2020-07-29 23:56:23 +0700

    add next JS to app

--- lereorg/src/components/Client/Header/HeaderCart/HeaderCart.jsx
+++ lereorg/src/components/Client/Header/HeaderCart/HeaderCart.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { makeStyles } from '@material-ui/core/styles';
 import faShoppingBag  from '@fortawesome/fontawesome-free-solid/faShoppingBag';
-import ShoppingBag from '../../../../assets/img/background/shopping_bag.svg';
-const HeaderCart = () => {
+ const HeaderCart = () => {
     return (
         <div>
             <div style={{background : `url(${ShoppingBag})`,height : "24px",width:"24px" }}>
