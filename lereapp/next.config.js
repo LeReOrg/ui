726a1b1f008e3180a721771c334245584c24df2b
Vinh Nguyen <vinhnq@ocb.com.vn>
2020-08-04 17:17:22 +0700

    change config webpack

--- lereapp/next.config.js
+++ lereapp/next.config.js
@@ -1,20 +1,2 @@
-module.exports = {
-    webpack: (config, options) => {
-        // Note: we provide webpack above so you should not `require` it
-        // Perform customizations to webpack config
-        config.module.rules.push({
-            
-                test: /\.(png|jpg|gif|svg)$/i,
-                use: [
-                {
-                    loader: 'url-loader',
-                    
-                },
-                ],
-            }
-            
-        )
-        // Important: return the modified config
-        return config
-    },
-}
+const withImages = require("next-images");
+module.exports = withImages();
