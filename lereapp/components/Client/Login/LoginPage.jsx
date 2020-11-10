e18905b35362a4337aaa4b7af6116b81d4515bbe
Vinh Nguyen <vinhnq@ocb.com.vn>
2020-11-10 09:58:39 +0700

    Them 2 page login

---
+++ lereapp/components/Client/Login/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/styles";
+
+const LoginPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LoginPage;
