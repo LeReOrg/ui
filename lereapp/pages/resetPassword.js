cc7c40bdcb9f9faebcbd9680dfd42f52eff79385
vinh nguyen <vinh1593570@gmail.com>
2020-12-20 22:12:15 +0700

    update reset password

---
+++ lereapp/pages/resetPassword.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ResetPasswordPage from "../components/Client/ForGotPassword/ResetPassword";
+const ResetPassWord = () => {
+  return <div><ResetPasswordPage /></div>;
+};
+
+export default ResetPassWord;
