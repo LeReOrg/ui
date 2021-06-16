d5942bf674eaaab1e64c2cadc32dff023a97be01
Vinh <vinh1593570@gmail.com>
2021-06-16 23:15:01 +0700

    styling

--- components/Client/Header/AccountList/AccountList.jsx
+++ components/Client/Header/AccountList/AccountList.jsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 import styles from "../ShowLogin/ShowLoginStyled";
 import { auth, logOut } from "../../../../firebase/firenase.utils";
 import { useRecoilState } from "recoil";
+import { useRouter } from "next/router";
 import { userState } from "../../../../lib/recoil-root";
 const AccountList = ({ user }) => {
   const useStyled = makeStyles(styles);
   const [currentUser, setCurrentUser] = useRecoilState(userState);
-
+  const router = useRouter();
   const logOutFuntion = (event) => {
     event.preventDefault();
     auth.signOut().then(() => {
       setCurrentUser("");
+      router.push("/");
     });
   };
   const classes = useStyled();
